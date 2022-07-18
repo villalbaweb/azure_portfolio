78e41c848f7c6cb7e202e1e7f0d7ba6a3204167c
Daniel Villalba <villalbaweb@gmail.com>
2022-07-18 18:43:26 -0500

    Refactoring, adding angular samples and csharp samples modules along with lazy loading routing

---
+++ Portfolio.App/ClientApp/src/app/angular-samples/angular-samples.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-samples',
+  templateUrl: './angular-samples.component.html',
+  styleUrls: ['./angular-samples.component.css']
+})
+export class AngularSamplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Portfolio.App/ClientApp/src/app/app.routes.ts
+++ Portfolio.App/ClientApp/src/app/app.routes.ts
@@ -8,4 +8,6 @@ export const AppRoutes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'counter', component: CounterComponent },
   { path: 'fetch-data', component: FetchDataComponent },
+  { path: 'angular-samples', loadChildren: () => import('./angular-samples/angular-samples.module').then(m => m.AngularSamplesModule) },
+  { path: 'csharp-samples', loadChildren: () => import('./csharp-samples/csharp-samples.module').then(m => m.CsharpSamplesModule) },
 ];

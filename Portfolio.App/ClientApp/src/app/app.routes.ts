79feeae5b9201e86020664b773d2fa93b6330e7a
Daniel Villalba <villalbaweb@gmail.com>
2022-07-18 18:12:18 -0500

    Refactoring to add app.routes.ts

---
+++ Portfolio.App/ClientApp/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
+export const AppRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'counter', component: CounterComponent },
+  { path: 'fetch-data', component: FetchDataComponent },
+];

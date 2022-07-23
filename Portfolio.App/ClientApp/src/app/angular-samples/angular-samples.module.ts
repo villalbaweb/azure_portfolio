8da35f2858c84b65f4b7587b55f2d43f4ecf99c0
Daniel Villalba <villalbaweb@gmail.com>
2022-07-23 13:39:52 -0500

    Reafctoring, adding proper ngx-bootstrap implementation

--- Portfolio.App/ClientApp/src/app/angular-samples/angular-samples.module.ts
+++ Portfolio.App/ClientApp/src/app/angular-samples/angular-samples.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { AngularSamplesComponent } from './angular-samples.component';
 
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
 
 const routes: Routes = [
   { path: '', component: AngularSamplesComponent }
@@ -14,7 +17,9 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    TooltipModule.forRoot(),
+    BsDatepickerModule.forRoot()
   ]
 })
 export class AngularSamplesModule { }
--- Portfolio.App/ClientApp/src/app/app.module.ts
+++ Portfolio.App/ClientApp/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutes } from './app.routes';
 
 import { AppComponent } from './app.component';
@@ -16,6 +18,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot(AppRoutes)

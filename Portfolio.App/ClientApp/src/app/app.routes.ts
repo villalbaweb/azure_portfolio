aaf13e3e5b85351a9f9136b241e30f99a7483da7
Daniel Villalba <villalbaweb@gmail.com>
2022-08-31 10:16:51 -0500

    Deleting not required components and its routes

--- Portfolio.App/ClientApp/src/app/app.routes.ts
+++ Portfolio.App/ClientApp/src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 export const AppRoutes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'counter', component: CounterComponent },
-  { path: 'fetch-data', component: FetchDataComponent },
   { path: 'angular-samples', loadChildren: () => import('./angular-samples/angular-samples.module').then(m => m.AngularSamplesModule) },
   { path: 'csharp-samples', loadChildren: () => import('./csharp-samples/csharp-samples.module').then(m => m.CsharpSamplesModule) },
 ];
